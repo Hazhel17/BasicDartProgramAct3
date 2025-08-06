fe23ba379fde5b8ce87e775affd4b5caa5ae649b
Convicto, Hazel Ann <hazelannconvicto@gmail.com>
2025-08-06 11:51:05 +0000

    Dart simple calculator program.

---
+++ Calculator.dart
@@ -0,0 +1,14 @@
+void main() {
+  int num1 = 17;
+  int num2 = 6;
+
+  int sum = num1 + num2;
+  int diff = num1 - num2;
+  int mul = num1 * num2;
+  double div = num1 / num2; 
+
+  print("The sum is $sum");
+  print("The difference is $diff");
+  print("The product is $mul");
+  print("The division is $div");
+}
